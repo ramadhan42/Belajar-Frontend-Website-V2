94a6fb366ad2ea745be9ad4e247e43aa3ea7ce17
ramadhanmuhammadnur295-glitch <ramadhanmuhammadnur295@gmail.com>
2026-06-09 15:01:09 +0700

    update 9 juni 2026, 15:01

--- app/halaman/kuis/page.tsx
+++ app/halaman/kuis/page.tsx
@@ -1,7 +1,15 @@
+import Footer from "@/components/global/Footer";
+import Navbar from "@/components/global/Navbar";
+
 export default function Kuis() {
     return (
-        <div>
-            <h1>Kuis</h1>
+        <div className="bg-[#1172BA] flex flex-col justify-center items-center min-h-screen">
+            <Navbar />
+
+            <div className="text-white text-3xl font-bold mt-20">
+                Halaman Kuis
+            </div>
+            <Footer />
         </div>
     );
 }
