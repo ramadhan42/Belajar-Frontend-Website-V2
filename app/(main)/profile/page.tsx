090940e1933c00e07b91aa9ac2159e86a7ce4fa2
ramadhanmuhammadnur295-glitch <ramadhanmuhammadnur295@gmail.com>
2026-06-26 16:54:41 +0700

    up url global 2

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -78,7 +78,7 @@ export default function ProfilePage() {
     }
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || "https://belajar-be-website-evomi-v2-main-gbcsym.free.laravel.cloud"}/user/profile`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || "https://belajar-be-website-evomi-v2-main-gbcsym.free.laravel.cloud/api"}/user/profile`, {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${token}`,
